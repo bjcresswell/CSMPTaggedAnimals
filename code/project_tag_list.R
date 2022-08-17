5db7d0a43ec48196827996540fe7d43b8c2a3b65
Ben Cresswell <benjamin.cresswell@jcu.edu.au>
2022-08-17 16:30:17 +1000

    Edits to tag assessments

--- code/project_tag_list.R
+++ code/project_tag_list.R
@@ -6,9 +6,9 @@
 ## If you want to run from within this script:
 ## setwd("code")
 
-#getwd()
+getwd()
 #rm(list=ls())
-setwd("../code")
+#setwd("../code")
 
 
 # Packages
@@ -20,15 +20,15 @@ library(tidyverse)
 
 # Load the 2 files and only retain the ID column
 # Sharks
-project_serials_sharks <- read_excel("../data/csmp_sharks.xlsx", trim_ws = TRUE) %>% 
+project_IDs_sharks <- read_excel("../data/csmp_sharks.xlsx", trim_ws = TRUE) %>% 
   dplyr::select(ID)
 # Teleosts
-project_serials_teleosts <- read_excel("../data/csmp_teleosts.xlsx", trim_ws = TRUE) %>% 
+project_IDs_teleosts <- read_excel("../data/csmp_teleosts.xlsx", trim_ws = TRUE) %>% 
   dplyr::select(ID)
 
 # Bind together
-project_serials <- project_serials_sharks %>% 
-  rbind(project_serials_teleosts) 
+project_IDs <- project_IDs_sharks %>% 
+  rbind(project_IDs_teleosts)
 
 
 # Then merge with the regional tag list to extract the rest of the metadata
@@ -37,17 +37,87 @@ project_serials <- project_serials_sharks %>%
 source("../code/regional_tag_list.R")
 
 
-project_tag_list <- project_serials %>% 
+# List of total project tags
+project_tags_all <- project_IDs %>% 
   merge(regional_tag_list, by = 'ID') # Adds in other variables we might need
+# 137 tags total (inc sensor tags)
 
-
-# List of project sharks
-project_sharks <- project_tag_list %>% 
-  filter(Org_type == 'Elasmobranch') %>% 
-  distinct(transmitter_id)
+# List of project tags in sharks
+project_tags_sharks <- project_tag_list %>% 
+  filter(Org_type == 'Elasmobranch') #%>% 
+  #distinct(transmitter_id)
 #write.csv(project_sharks, file = '../data/RData/project_sharks.csv')
+# 114 tags in sharks total (inc sensor tags)
 
-# List of project teleosts
-project_teleosts <- project_tag_list %>% 
-  filter(Org_type == 'Teleost') %>% 
-  distinct(transmitter_id)
+# List of project tags in  teleosts
+project_tags_teleosts <- project_tag_list %>% 
+  filter(Org_type == 'Teleost')# %>% 
+  #distinct(transmitter_id)
+# 23 tags in telosts total (inc sensor tags)
+
+
+
+# Just one entry per animal
+## Should just be able to get rid of one of the sensor transmitters (e.g. pressure)
+
+# All animals
+project_serials <- 
+  project_tag_list %>%
+  filter(Type != "press")
+# 112 total
+
+project_serials_sharks <- 
+  project_tags_sharks %>%
+  filter(Type != "press")
+# 99 total
+
+project_serials_teleosts <- 
+  project_tags_teleosts %>%
+  filter(Type != "press")
+# 13 total
+  
+# 137 - 112 = 25 animals with tags in: 10 teleost and 15 sharks
+# Can check:
+sensortag_organisms <- 
+project_tag_list %>% 
+  filter(Type %in% c("press", "temp"))
+
+# By reef
+
+## Osprey
+project_serials %>% 
+  filter(Location == 'Osprey') %>% 
+  droplevels() %>% 
+  summary()
+## 50 - 13 teleosts, 37 elasmobranchs
+
+## Bougainville 
+project_serials %>% 
+  filter(Location == 'Bougainville') %>% 
+  droplevels() %>% 
+  summary()
+# 9
+
+
+## Holmes
+project_serials %>% 
+  filter(str_detect(Location, 'Holmes')) %>% 
+  droplevels() %>% 
+  summary()
+
+
+## Flinders
+project_serials %>% 
+  filter(str_detect(Location, 'Flin')) %>% 
+  droplevels() %>% 
+  summary()
+
+
+
+
+
+
+
+
+
+
