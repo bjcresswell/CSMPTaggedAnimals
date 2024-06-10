73bec557f9d1405167fbe0eb6d7c5d64551e8952
Ben Cresswell <bjcresswell@gmail.com>
2024-06-10 21:04:02 +1000

    Add packages script

---
+++ code/packages.R
@@ -0,0 +1,38 @@
+## Packages required 
+
+### Wrangling
+library(hms)
+library(readxl)
+library(plotrix)    # for SEM calc 
+library(lubridate)  # dates
+library(magrittr)
+library(sourceRmd)
+library(tidyverse)
+
+# Oceanography
+library(oce)
+library(ocedata)
+
+# Plotting
+library(patchwork)
+library(sjPlot)
+library(ggpubr)
+
+### Modeling
+library(glmmTMB)
+library(effects)   #for partial effects plots
+library(modelr)
+library(MASS)
+library(mgcv)
+library(brms)       #for fitting models in STAN
+library(rstan)
+library(coda)       #for diagnostics
+library(bayesplot)  #for diagnostics
+library(emmeans)    #for marginal means etc
+library(broom)      #for tidying outputs
+library(broom.mixed)
+library(DHARMa)     #for residual diagnostics
+library(MuMIn)
+library(tidybayes)  #for more tidying outputs
+library(ggeffects)  #for partial plots
+
